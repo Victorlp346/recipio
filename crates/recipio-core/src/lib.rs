fcbedf6de8d8dd77f7a233b9424cc63c95dc25b2
Victor Lopez <victorlp346@outlook.com>
2026-05-18 23:26:59 -0600

    Initial user implementation and moving towards being type driven

--- crates/recipio-core/src/lib.rs
+++ crates/recipio-core/src/lib.rs
@@ -3,14 +3,60 @@ mod recipe;
 mod units;
 mod user;
 
-use snafu::prelude::*;
+use std::marker::PhantomData;
+use std::str::FromStr;
+
+use thiserror::Error;
+use uuid::Uuid;
 
 pub use ingredient::{Ingredient, IngredientBuilder, IngredientRepository};
-
 pub use units::{MeasurementCategory, SubjectiveUnit, VolumeUnit, WeightUnit};
 
-#[derive(Debug, Snafu)]
-pub enum RecipioError {
-    #[snafu(display("An error ocurred on the repostory"))]
-    RepositoryError,
+#[derive(Debug, Clone)]
+pub struct Id<T>(Uuid, PhantomData<T>);
+
+impl<T> Id<T> {
+    pub fn new() -> Self {
+        Self(Uuid::now_v7(), PhantomData)
+    }
 }
+
+impl<T> Default for Id<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> From<Uuid> for Id<T> {
+    fn from(raw: Uuid) -> Self {
+        Self(raw, PhantomData)
+    }
+}
+
+impl<T> FromStr for Id<T> {
+    type Err = RecipioError;
+
+    fn from_str(raw: &str) -> Result<Self, Self::Err> {
+        let id = Uuid::from_str(raw).map_err(|_| RecipioError::ParsingError {
+            value: raw.to_string(),
+            target: String::from("uuid"),
+        })?;
+        Ok(Self(id, PhantomData))
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum RecipioError {
+    #[error("repository error")]
+    Repo(#[from] RepoError),
+    #[error("impossible to parse {value:?} to {target:?}")]
+    ParsingError { value: String, target: String },
+}
+
+#[derive(Debug, Error)]
+pub enum RepoError {
+    #[error("Unknown repository error")]
+    UnknownError,
+}
+
+pub type RepoResult<T> = Result<T, RepoError>;
--- crates/recipio-core/src/user.rs
+++ crates/recipio-core/src/user.rs
@@ -1,8 +1,82 @@
-use uuid::Uuid;
+use crate::{Id, RecipioError, RepoResult};
+use async_trait::async_trait;
+use derive_more::{Display, From};
+use std::{fmt::Debug, str::FromStr};
 
+#[derive(Display, Clone, Debug)]
+pub struct Username(String);
+
+impl FromStr for Username {
+    type Err = RecipioError;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        if value.len() < 3 {
+            return Err(RecipioError::ParsingError {
+                value: value.to_string(),
+                target: "Username (min 3 chars)".to_string(),
+            });
+        }
+        Ok(Self(value.to_string()))
+    }
+}
+
+#[derive(Display, Clone, Debug)]
+pub struct Email(String);
+
+impl FromStr for Email {
+    type Err = RecipioError;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        if !value.contains('@') {
+            return Err(RecipioError::ParsingError {
+                value: value.to_string(),
+                target: "Email (must contain @)".to_string(),
+            });
+        }
+        Ok(Self(value.to_string()))
+    }
+}
+
+#[derive(From, Clone)]
+pub struct HashedPassword(String);
+
+impl Debug for HashedPassword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("HashedPassword(*****)")
+    }
+}
+
+//TODO: Add password rule validations
+#[derive(From, Clone)]
+pub struct UnhashedPassword(String);
+
+impl Debug for UnhashedPassword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("UnhashedPassword(****)")
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct User {
-    id: Uuid,
-    username: String,
-    email: String,
-    
+    id: Id<User>,
+    username: Username,
+    email: Email,
+    password: HashedPassword,
+}
+
+#[derive(Debug, Clone)]
+pub struct CreateUserDTO {
+    username: Username,
+    email: Email,
+    password: UnhashedPassword,
+}
+
+#[derive(Debug, Clone)]
+pub struct RetrieveUserDTO {
+    id: Id<User>,
+    username: Username,
+    email: Email,
+}
+
+#[async_trait]
+pub trait UserRepository {
+    async fn add(user: User) -> RepoResult<User>;
 }
